f1b30f539e772d35262f2754c69e0a1aee554339
juliabosak <1kn15b.bosak@gmail.com>
2019-10-07 15:49:18 +0300

    add project configurations files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Contacts/>
+  </div>
+</template>
+
+<script>
+import Contacts from './components/contacts.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Contacts
+  }
+}
+</script>
+
+<style>
+</style>
