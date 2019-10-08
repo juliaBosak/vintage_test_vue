4dbeedc6a209a4b79ccad1286d6bf04cacbbe750
juliabosak <1kn15b.bosak@gmail.com>
2019-10-09 02:25:49 +0300

    add send form

--- src/components/contacts/componets/contactsForm.vue
+++ src/components/contacts/componets/contactsForm.vue
@@ -6,18 +6,18 @@
       </header>
       <div class="contacts__row">
             <form id="contactsForm"
-                  action="http://httpbin.org/post"
-                  method="post"
+                  @submit.prevent="sentForm()"
                   class="contacts__form form">
               <div class="form__field-wrap">
                   <input
                     class="form__field"
                     type="text"
                     v-model="name"
+                    id="name"
                     v-bind:class="{ error: isNameError }"
                     @blur="isNameTouched = true"
                   >
-                <label class="form__label">Name</label>
+                <label for="name" class="form__label">Name</label>
                 <div class="form__error-message" v-if="isNameError">
                   Enter your name
                 </div>
@@ -27,10 +27,11 @@
                     class="form__field"
                     type="text"
                     v-model="phone"
+                    id="phone"
                     v-bind:class="{ error: isPhoneError }"
                     @blur="isPhoneTouched = true"
                    >
-                <label class="form__label">Phone</label>
+                <label for="phone" class="form__label">Phone</label>
                 <div class="form__error-message" v-if="isPhoneError">
                   Enter your phone
                 </div>
@@ -40,33 +41,41 @@
                     class="form__field"
                     type="text"
                     v-model="email"
+                    id="email"
                     v-bind:class="{ error: isEmailError }"
                     @blur="isEmailTouched = true">
-                <label class="form__label">E-mail</label>
+                <label for="email" class="form__label">E-mail</label>
                 <div class="form__error-message" v-if="isEmailError">
                   Enter your email
                 </div>
 
               </div>
               <div class="form__checkbox-wrap">
+                <label class="checkbox">
                   <input
-                    type="checkbox"
-                    class="form__checkbox"
-                    v-model="isAgree"
-                    @blur="isCheckboxTouched = true"
-                    v-bind:class="{ error: !isAgree && isCheckboxTouched }"
+                  type="checkbox"
+                  class="form__checkbox checkbox-input"
+                  v-model="isAgree"
+                  @blur="isCheckboxTouched = true"
+                  v-bind:class="{ error: isNotAgree }"
                   >
-                <label class="form__checkbox-label">I agree the processing of personal data</label>
-                <div class="form__error-message" v-if="!isAgree && isCheckboxTouched">
+                  <span class="form__checkbox-label checkbox-label">I agree the processing of personal data</span>
+                </label>
+                <div class="form__error-message" v-if="isNotAgree">
                   Please, agree the processing of personal data
                 </div>
+              </div>
+              <div class="form__success"
+                   v-if="statusOk">
+                Form has sent
               </div>
               <div class="form__button-wrap">
                 <button
                   class="form__button"
-                  :disabled="false"
+                  :disabled="isSubmiting"
                 type="submit">Get in touch</button>
               </div>
+
             </form>
             <div class="contacts__form-description">
               <p class="contacts__form-text">
@@ -79,8 +88,8 @@
 
 </template>
 <script>
-const emailCheckRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const phoneCheckRegex = /[0][1-9]{9}/;
+const emailCheckRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const phoneCheckRegex = /[0][1-9]{9}/
 export default {
   name: 'Form',
   data () {
@@ -92,7 +101,9 @@ export default {
       isEmailTouched: false,
       isNameTouched: false,
       isPhoneTouched: false,
-      isCheckboxTouched: false
+      isCheckboxTouched: false,
+      isSubmiting: false,
+      statusOk: false
     }
   },
   computed: {
@@ -108,8 +119,57 @@ export default {
     isPhoneError () {
       return !this.isPhoneValid && this.isPhoneTouched
     },
+    isNameValid () {
+      return this.name
+    },
     isNameError () {
-      return !this.name && this.isNameTouched
+      return !this.isNameValid && this.isNameTouched
+    },
+    isAgreeValid () {
+      return this.isAgree
+    },
+    isNotAgree () {
+      return !this.isAgreeValid && this.isCheckboxTouched
+    }
+  },
+  methods: {
+    sentForm: function () {
+      this.isSubmiting = true
+      if (this.isEmailValid && this.isNameValid && this.isPhoneValid && this.isAgreeValid) {
+        postData('http://httpbin.org/post',
+          {
+            name: this.name,
+            phone: this.phone,
+            email: this.email,
+            isAgree: this.isAgree
+          })
+          .then(data => {
+            console.log(JSON.stringify(data))
+            this.isSubmiting = false
+            this.statusOk = true
+            setTimeout(() => { this.statusOk = false }, 1000)
+          })
+          .catch(error => {
+            console.error(error)
+            this.isSubmiting = false
+          })
+      }
+
+      function postData (url = '', data = {}) {
+        return fetch(url, {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          redirect: 'follow',
+          referrer: 'no-referrer',
+          body: JSON.stringify(data)
+        })
+          .then(response => response.json())
+      }
     }
   }
 
